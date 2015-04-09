ff61aa71e6073753e4a56b51285b9fd739a153c6
Stephen McCullough <stephen@sixdegreelabs.com>
2015-04-10 00:24:02 +0100

    Added in basic usage for the stats script

--- git/repo-stats.sh
+++ git/repo-stats.sh
@@ -1,6 +1,25 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash -e
 
-set -e 
+#----------------------------------------------------------------------
+# Script gives you the following information for a git repo:
+# 
+#  [no of lines commited] [hash] [commit meesages]
+#
+#     ➜  git git:(master) ✗ ./repo-stats.sh 
+#     68 1919a47 added python directory and the csv to json script
+#     94 e5dd322 Added repo-stats.sh
+#     97 588e404 Added in regex argument but not sure if it is working correctly
+#     97 50b2b3c Amended some stupid indentation
+#
+# You can add in a regex to give you information. So if you wanted to # see all 
+# commits that were for ruby files then use '.rb' or tests then 'tests'
+#
+#     ➜  git git:(master) ✗ ./repo-stats.sh '.sh' 
+#     0 1919a47 added python directory and the csv to json script
+#     26 e5dd322 Added repo-stats.sh
+#     29 588e404 Added in regex argument but not sure if it is working correctly
+#     29 50b2b3c Amended some stupid indentation
+#---------------------------------------------------------------------- 
 
 pattern=$1
 
